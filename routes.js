20ac2fe71b197e557981ada8a94fd241bd9690c6
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2021-11-09 10:26:25 +0000

    Setup View Engine + Workspace Changes

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const express = require(`express`);
 const router = express.Router()
 
 router.get(`/`, (req,res) => {
-    res.send(`Hello World`);
+    res.render(`homepage`);
 })
 
 //Export the router
