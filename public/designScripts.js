e4e79d42cfb8781a9e0580e8f9230441af81d5b6
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2022-01-03 15:06:46 +0000

    Save before working out how to save boat designs

--- public/designScripts.js
+++ public/designScripts.js
@@ -2,11 +2,34 @@ let lastClickedSide = null;
 let lastClickedRow = null;
 let previouslyClicked = false;
 function boxClicked(boxSide,boxIndex) {
-    console.log(boxIndex + boxSide)
     if (previouslyClicked) {
+        if (boxIndex !== lastClickedRow || boxSide !== lastClickedRow) {
+            // var x=new XMLHttpRequest();
+            // x.open("patch", "/boatDesignSwap");
+            // x.send({"pos1Row": lastClickedRow, "pos1Side": lastClickedSide, "pos2Row": boxIndex, "pos2Side": boxSide});
 
+            const options = { 
+                method: 'PATCH',
+                headers: {
+                  'Accept': 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({"pos1Row": lastClickedRow, "pos1Side": lastClickedSide, "pos2Row": boxIndex, "pos2Side": boxSide})
+              }    
+              
+              fetch(window.location.href+"&type=swap", options)
+                .then(response => {
+                   if (response.ok) {
+                       console.log(response.json());
+                     } else {
+                        throw new Error('Something went wrong ...');
+                     }
+                })
+
+        }
     } else {
         lastClickedSide = boxSide;
         lastClickedRow = boxIndex;
+        previouslyClicked = true;
     }
 }
